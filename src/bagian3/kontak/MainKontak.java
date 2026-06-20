fa594f34c4c3eec28e0e03a9734069b96778f389
NadiaIntania <nadiaintania37@gmail.com>
2026-06-20 13:24:26 +0700

    Selesai Bagian 3: Multi-Class

---
+++ src/bagian3/kontak/MainKontak.java
@@ -0,0 +1,30 @@
+package bagian3.kontak;
+
+public class MainKontak {
+    public static void main(String[] args) {
+        BukuKontak buku = new BukuKontak("kontak.txt");
+        buku.tambahKontak(new Kontak("Andi","0811111","andi@gmail.com"));
+        buku.tambahKontak(new Kontak("Budi","0822222","budi@gmail.com"));
+        buku.tambahKontak(new Kontak("Citra","0833333","citra@gmail.com"));
+
+        buku.tampilkanSemua();
+        buku.simpanKeBerkas();
+
+        System.out.println();
+
+        BukuKontak bukuLain = new BukuKontak("kontak.txt");
+        bukuLain.muatDariBerkas();
+        bukuLain.tampilkanSemua();
+
+        System.out.println("Jumlah kontak: "+ bukuLain.jumlahKontak());
+        System.out.println();
+
+        bukuLain.cariKontak("Budi");
+        System.out.println();
+
+        bukuLain.hapusKontak("Budi");
+        System.out.println("\nSetelah dihapus:");
+
+        bukuLain.tampilkanSemua();
+    }
+}
